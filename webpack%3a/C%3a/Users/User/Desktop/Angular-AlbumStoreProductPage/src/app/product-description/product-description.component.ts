b8076d8a83e5ec6c0878d4ebdf0b0c22eb880b5c
Dev2Ops2 <dev2ops2@hotmail.com>
2021-02-08 13:16:01 +0100

    Completed module

--- webpack%3a/C%3a/Users/User/Desktop/Angular-AlbumStoreProductPage/src/app/product-description/product-description.component.ts
+++ webpack%3a/C%3a/Users/User/Desktop/Angular-AlbumStoreProductPage/src/app/product-description/product-description.component.ts
@@ -11,10 +11,13 @@ export class ProductDescriptionComponent implements OnInit {
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(response => {
-       this.albumInfo = response;
-       console.log(this.albumInfo);
-    }
+    this._productService.getAlbum(1).subscribe(response =>
+     this.albumInfo = response);
   }
 
 }
+
+
+
+// WEBPACK FOOTER //
+// C:/Users/User/Desktop/Angular-AlbumStoreProductPage/src/app/product-description/product-description.component.ts
