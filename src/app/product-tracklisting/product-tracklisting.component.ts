c876a77c0d996b0335476f7853553be1e3ad677c
Dev2Ops2 <dev2ops2@hotmail.com>
2021-02-08 18:29:13 +0100

    Completed module 6

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -16,7 +16,7 @@ export class ProductTracklistingComponent implements OnInit {
 
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(response =>
-      this.albumInfo = response
+      this.albumInfo = response)
   }
 
 }
